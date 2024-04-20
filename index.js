51e043fac84d62a63807bbfbffea8218d70886b0
Asad9340 <asadulimran1999@gmail.com>
2024-04-20 23:43:33 +0600

    created user in the index file

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const users = [
+  {
+    id: 1,
+    name: 'Asad',
+    email:'asad@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Emran',
+    email:'emran@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Emon',
+    email:'emon@gmail.com',
+  }
+]
 app.get('/', (req, res) => {
   res.send('Hello there ,This is my first server project run 1');
 })
