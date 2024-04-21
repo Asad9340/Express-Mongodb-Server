75ccde20afbd5622dd223b7dd71bc2223c36c88c
Asad9340 <asadulimran1999@gmail.com>
2024-04-21 12:46:19 +0600

    changes some code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 const users = [
   {
     id: 1,
@@ -18,16 +21,29 @@ const users = [
     email: 'emon@gmail.com',
   },
 ];
-    app.get('/', (req, res) => {
-      res.send('Hello there ,This is my first server project run 1');
-    });
-
+app.get('/', (req, res) => {
+  res.send('Hello there ,This is my first server project run 1');
+});
 
 app.get('/users', (req, res) => {
   res.send(users);
-})
+});
+
+app.post('/users', (req, res) => {
+  console.log('Post request hitting');
+  console.log(req.body);
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ error: 'Request body is missing or invalid' });
+  }
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
 
 
-    app.listen(port, () => {
-      console.log(`the app is listening on port ${port}`);
-    });
+app.listen(port, () => {
+  console.log(`the app is listening on port ${port}`);
+});
