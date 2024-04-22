1dc64b7b2fcdb281eab59f7bc6b5644535aa5e4c
Asad9340 <asadulimran1999@gmail.com>
2024-04-22 11:24:59 +0600

    change code for new setup for mongodb

--- index.js
+++ index.js
@@ -3,47 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
-const users = [
-  {
-    id: 1,
-    name: 'Asad',
-    email: 'asad@gmail.com',
-  },
-  {
-    id: 2,
-    name: 'Emran',
-    email: 'emran@gmail.com',
-  },
-  {
-    id: 3,
-    name: 'Emon',
-    email: 'emon@gmail.com',
-  },
-];
-app.get('/', (req, res) => {
-  res.send('Hello there ,This is my first server project run 1');
-});
+app.use(express.json());
 
-app.get('/users', (req, res) => {
-  res.send(users);
-});
-
-app.post('/users', (req, res) => {
-  console.log('Post request hitting');
-  console.log(req.body);
-  if (!req.body) {
-    return res
-      .status(400)
-      .json({ error: 'Request body is missing or invalid' });
-  }
-  const newUser = req.body;
-  newUser.id = users.length + 1;
-  users.push(newUser);
-  res.status(201).json(newUser);
-});
-
-
-app.listen(port, () => {
-  console.log(`the app is listening on port ${port}`);
-});
