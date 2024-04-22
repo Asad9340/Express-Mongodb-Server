5e41799345747ef6d7c9a29390ed6a645b0b5e87
Asad9340 <asadulimran1999@gmail.com>
2024-04-22 11:34:19 +0600

    server site is complete for running

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  app.send('Simple project is running');
+})
+
+app.listen(port, () => {
+  console.log(`Project is running on ${port}`)
+})
